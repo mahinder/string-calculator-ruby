83815e42f4577631313771c6e16493aca1d29889
mahinder <mahinder124@gmail.com>
2025-07-22 14:56:49 +0530

    Fail - Raise error on negative numbers, code not present.

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,5 +25,9 @@ RSpec.describe StringCalculator do
     it 'supports custom delimiter' do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises error for negative numbers' do
+      expect { StringCalculator.add("1,-2") }.to raise_error("negative numbers not allowed -2")
+    end
   end
 end
