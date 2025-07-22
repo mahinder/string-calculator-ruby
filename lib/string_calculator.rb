3566dd76a47e4205f78ea5a7b288fc36e975aadd
mahinder <mahinder124@gmail.com>
2025-07-22 15:00:37 +0530

    Pass raise error on negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,17 +1,22 @@
 class StringCalculator
-    
   def self.add(input)
-   return 0 if input.empty?
-   delimiter = /,|\n/
-   
-   if input.start_with?("//")
-    parts = input.split("\n", 2)
-    custom_delim = Regexp.escape(parts[0][2..])
-    delimiter = /#{custom_delim}/
-    input = parts[1]
-   end
+    return 0 if input.empty?
+    delimiter = /,|\n/
 
-   numbers = input.split(delimiter)
-   numbers.map(&:to_i).sum
+    if input.start_with?("//")
+      parts = input.split("\n", 2)
+      custom_delim = Regexp.escape(parts[0][2..])
+      delimiter = /#{custom_delim}/
+      input = parts[1]
+    end
+
+    numbers = input.split(delimiter).map(&:to_i)
+    negatives = numbers.select { |n| n < 0 }
+
+    unless negatives.empty?
+      raise "negative numbers not allowed #{negatives.join(",")}"
+    end
+
+    numbers.sum
   end
 end
