7ec81894e1793f8f3f89b6ee9563d56c07ba55c9
mahinder <mahinder124@gmail.com>
2025-07-22 14:52:41 +0530

    pass Support custom delimiter //;\n1;2

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,17 @@
 class StringCalculator
+    
   def self.add(input)
    return 0 if input.empty?
-   numbers = input.split(/,|\n/)
+   delimiter = /,|\n/
+   
+   if input.start_with?("//")
+    parts = input.split("\n", 2)
+    custom_delim = Regexp.escape(parts[0][2..])
+    delimiter = /#{custom_delim}/
+    input = parts[1]
+   end
+
+   numbers = input.split(delimiter)
    numbers.map(&:to_i).sum
   end
 end
