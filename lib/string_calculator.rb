c229754249250310e0d11e8163bd35d2350ef928
mahinder <mahinder124@gmail.com>
2025-07-22 16:44:32 +0530

    Handle Input errors

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,24 +5,44 @@ class StringCalculator
   class NegativeNumberError < StandardError; end
   class InvalidInputFormatError < StandardError; end
 
+  DEFAULT_DELIMITER = /,|\n/.freeze
+
   def self.add(input)
     return 0 if input.nil? || input.strip.empty?
 
-    delimiter = /,|\n/
-    if input.start_with?("//")
-      parts = input.split("\n", 2)
-      custom_delim = Regexp.escape(parts[0][2..])
-      delimiter = /#{custom_delim}/
-      input = parts[1]
-    end
+    delimiter, numbers_string = extract_delimiter_and_numbers(input)
+    numbers = parse_numbers(numbers_string, delimiter)
 
-    numbers = input.split(delimiter).map(&:to_i)
-    negatives = numbers.select { |n| n < 0 }
-
-    if negatives.any?
-      raise "negative numbers not allowed #{negatives.join(",")}"
-    end
+    check_for_negatives(numbers)
 
     numbers.sum
+  end
+
+  private
+
+  def self.extract_delimiter_and_numbers(input)
+    if input.start_with?("//")
+      header, body = input.split("\n", 2)
+      raise InvalidInputFormatError, "Missing numbers after delimiter declaration" unless body
+
+      custom_delim = header[2..]
+      raise InvalidInputFormatError, "Invalid custom delimiter format" if custom_delim.strip.empty?
+
+      [Regexp.new(Regexp.escape(custom_delim)), body]
+    else
+      [DEFAULT_DELIMITER, input]
+    end
+  end
+  def self.parse_numbers(numbers_string, delimiter)
+    numbers_string.split(delimiter).map do |n|
+      Integer(n)
+    rescue ArgumentError
+      raise InvalidInputFormatError, "Invalid number: '#{n}'"
+    end
+  end
+
+  def self.check_for_negatives(numbers)
+    negatives = numbers.select(&:negative?)
+    raise NegativeNumberError, "negative numbers not allowed #{negatives.join(",")}" unless negatives.empty?
   end
 end
