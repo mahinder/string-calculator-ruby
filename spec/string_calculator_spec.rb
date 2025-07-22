625b47402779739e2294e8c4065e135c1ec16f96
mahinder <mahinder124@gmail.com>
2025-07-22 15:01:33 +0530

    Add test Multiple negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,33 +1,37 @@
-require 'string_calculator'
+require "string_calculator"
 
 RSpec.describe StringCalculator do
-  describe '.add' do
-    it 'returns 0 for empty string' do
+  describe ".add" do
+    it "returns 0 for empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
 
-    it 'returns the number for a single input' do
+    it "returns the number for a single input" do
       expect(StringCalculator.add("4")).to eq(4)
     end
 
-    it 'returns sum of two comma-separated numbers' do
+    it "returns sum of two comma-separated numbers" do
       expect(StringCalculator.add("1,2")).to eq(3)
     end
 
-    it 'returns sum of multiple comma-separated numbers' do
+    it "returns sum of multiple comma-separated numbers" do
       expect(StringCalculator.add("1,2,3")).to eq(6)
     end
 
-    it 'handles newlines as delimiters' do
+    it "handles newlines as delimiters" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
 
-    it 'supports custom delimiter' do
+    it "supports custom delimiter" do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
 
-    it 'raises error for negative numbers' do
+    it "raises error for negative numbers" do
       expect { StringCalculator.add("1,-2") }.to raise_error("negative numbers not allowed -2")
+    end
+
+    it "lists all negative numbers in error" do
+      expect { StringCalculator.add("-1,-2,3") }.to raise_error("negative numbers not allowed -1,-2")
     end
   end
 end
